31748fe4d271a2da1ab2bc68ddc8e6be7d009953
12345 <12345>
2021-09-28 17:20:19 +0800

    start

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="links">
+      <router-link to="/">首頁</router-link>
+      <router-link to="/about">列表</router-link>
+    </div>
   </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@for $i from 1 to 7 {
+  h#{$i}{
+    margin: 0;
+    font-size: ( 7 - $i ) * 0.25em + 1em;
   }
 }
+
+body{
+  margin: 0;
+}
+
+#nav{
+  background: #e61911;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.links{
+  a{
+    font-size: 20px;
+    color: #fff;
+    margin-right: 12px;
+    padding: 4px;
+    text-decoration: none;
+  }
+}
+
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p v-for="(item, index) in games" :key='index'>{{item}}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      games: [],
+    };
+  },
+  methods: {
+    getData() {
+      const source = 'https://sheets.googleapis.com/v4/spreadsheets/1ZAFezd3cItauJwNMB4mRqTHOck_8usjE5RFcgzowiso/values/遊戲列表?alt=json&key=AIzaSyA1tZh-XNAsEOc4sxWjyGoFznw-JlITBpE';
+      this.$http.get(source)
+        .then((res) => {
+          this.games = res.data.values;
+        });
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
